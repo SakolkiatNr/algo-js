76eef901e6cbbdd729d7d68b61a1141451d3357c
Sakolkiat <sakolkiat.n@gmail.com>
2025-09-23 05:48:56 +0700

    Add set hash value function

--- hashMap/hashMap.js
+++ hashMap/hashMap.js
@@ -1,23 +1,54 @@
-
 class HashMap {
-
-	constructor() {
-		this.capacity = 0;
-		this.loadFactor = 0;
+	constructor(initLoadFactor = 0.8, initCap = 16) {
+		// loadFactor = items / capacity
+		this.loadFactor = initLoadFactor;
+		this.capacity = initCap;
+		this.bucket = new Array(this.capacity).fill(null);
 	}
 
 	hash(key) {
-		let hashCode = 0;
-		// table size should ideally be a prime number to
+		// table size should ideally be a prime number to reduce collision
 		const primeNumber = 31;
+		let hashCode = 0;
 
 		for (let i = 0; i < key.length; i++) {
 			hashCode = primeNumber * hashCode + key.charCodeAt(i);
+			hashCode = hashCode % this.capacity;
 		}
 		return hashCode;
+	}
 
+	set(key, value) {
+		// check loadFactor
+		if (this.bucket.length > this.loadFactor * this.capacity) {
+			// increase capacity ()
+		}
+
+		let index = this.hash(key);
+
+		if (this.bucket[index] == null) {
+			this.bucket[index] = [];
+			this.bucket[index].push([key, value]);
+		} else {
+			// check item in sub-array
+			let item = this.bucket[index].find((pair) => pair[0] === key);
+
+			// if key existed 
+			if (item) item[1] = value;
+			else {
+				this.bucket[index].push([key, value]);
+			}
+		}
 	}
 
 }
 
+let map = new HashMap();
 
+map.set('lmao', 'value1');
+map.set('yahoo', 'old');
+map.set('yahoo', 'new');
+map.set('yahoo', 'newer');
+map.set('yaw', 'yaranaika!');
+
+console.log(map.bucket);
